9cd55e1f363dd58781d4180a73bbf20a7353c499
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-12-14 22:15:25 +0100

    in my main.cpp i created randomwords function and i call the BTS tree function i can calculate time and sorting words

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <cstdlib>
+#include "AVL.h"
+#include "BST.h"
+#include <ctime>
+
+std::vector<std::string>randomWords(int numw, int worldlenght){
+   std::vector<std::string > words;
+    srand(static_cast<unsigned int>(time(nullptr)));
+    for (int i=0;i<numw;i++){
+        std::string word;
+        for (int j=0;j<worldlenght;j++){
+            char randomchar='a'+rand()%26;
+            word+=randomchar;
+        }
+        words.push_back(word);
+    }
+    return words;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int numw=10000;
+    int worldlenght;
+    std::vector<std::string> randomwords= randomWords(numw,worldlenght);
+
+    std::cout<<"BST tree: ";
+    auto sbst=std::chrono::high_resolution_clock::now();
+    Bst bst;
+    for(const auto& word:randomwords){
+        bst.insert(word);
+        std::cout<<word<<" ";
+    }
+    std::cout<<std::endl;
+    auto ebst=std::chrono::high_resolution_clock::now();
+    auto durationbst=std::chrono::duration_cast<std::chrono::nanoseconds>(ebst-sbst).count();
+    std::cout<<"BTS tree time: "<<durationbst;
+
     return 0;
 }
