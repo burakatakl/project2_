c2767703684cdd55d1a5db007af3ba3694662433
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-12-15 22:44:21 +0100

    .

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@
 #include "AVL.h"
 #include "BST.h"
 #include <ctime>
-
-std::vector<std::string>randomWords(int numw, int worldlenght){
-   std::vector<std::string > words;
+void seedRandomizer(){
     srand(static_cast<unsigned int>(time(nullptr)));
+
+}
+std::vector<std::string>randomWords(int numw, int worldlength){
+   std::vector<std::string > words;
     for (int i=0;i<numw;i++){
         std::string word;
-        for (int j=0;j<worldlenght;j++){
+        for (int j=0;j<worldlength;j++){
             char randomchar='a'+rand()%26;
             word+=randomchar;
         }
@@ -22,10 +24,10 @@ std::vector<std::string>randomWords(int numw, int worldlenght){
 }
 
 int main() {
-
-    int numw=10;
-    int worldlenght=5;
-    std::vector<std::string> randomwords= randomWords(numw,worldlenght);
+    seedRandomizer();
+    const int numw=10;
+    const int worldlength=5;
+    std::vector<std::string> randomwords= randomWords(numw,worldlength);
 
     std::cout<<"BST tree: ";
     auto sbst=std::chrono::high_resolution_clock::now();
@@ -34,22 +36,22 @@ int main() {
         bst.insert(word);
         std::cout<<word<<" ";
     }
-    std::cout<<std::endl;
-    auto ebst=std::chrono::high_resolution_clock::now();
-    auto durationbst=std::chrono::duration_cast<std::chrono::nanoseconds>(ebst-sbst).count();
+    std::cout<<"\n";
+    const auto ebst=std::chrono::high_resolution_clock::now();
+    const auto durationbst=std::chrono::duration_cast<std::chrono::nanoseconds>(ebst-sbst).count();
     std::cout<<"BTS tree time: "<<durationbst;
-    std::cout<<std::endl;
+    std::cout<<"\n";
 
     std::cout<<"AVL tree: ";
-    auto savl=std::chrono::high_resolution_clock ::now();
+    const auto savl=std::chrono::high_resolution_clock ::now();
     avl avltree;
     for(const auto& word:randomwords){
         avltree.insert(word);
         std::cout<<word<<" ";
     }
-    std::cout<<std::endl;
-    auto eavl=std::chrono::high_resolution_clock ::now();
-    auto durationavl=std::chrono::duration_cast<std::chrono::nanoseconds>(eavl-savl).count();
+    std::cout<<"\n";
+    const auto eavl=std::chrono::high_resolution_clock ::now();
+    const auto durationavl=std::chrono::duration_cast<std::chrono::nanoseconds>(eavl-savl).count();
     std::cout<<"AVL tree time: "<<durationavl;
 
     return 0;
